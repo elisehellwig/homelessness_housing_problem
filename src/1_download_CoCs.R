4fc40801a60e62533c7a52608926943973f54c53
elisehellwig <echellwig@ucdavis.edu>
2025-08-05 12:39:43 -0500

    fixing variable names and making sure files get deleted

--- src/1_download_CoCs.R
+++ src/1_download_CoCs.R
@@ -1,7 +1,7 @@
 #' This script downloads Continuum of Care data from HUD. To download the 
 #' geometries of all CoCs for a particular year use 1_download_CoCs_national.sh.
 #' To download the Point in Time count data for a year or set of years, use
-#' 1_download_CoCs_timeseries.sh. 
+#' 1_download_CoCs_timeseries.sh. Requires terra package
 
 #Example files
 # https://files.hudexchange.info/reports/published/CoC_GIS_State_Shapefile_CA_2019.zip
@@ -23,8 +23,6 @@ message(msg)
 
 # Setup -------------------------------------------------------------------
 
-if (download_state) library('terra')
-
 spatial_path = 'data/spatial/'
 
 data_path = file.path(spatial_path, yr)
@@ -43,6 +41,15 @@ rsleep = function(min=1, max=25) {
   invisible()
 }
 
+download_and_delete = function(url, fn, ex_path) {
+  download.file(url, fn)
+  rsleep()
+  
+  unzip(fn, exdir=ex_path)
+  
+  unlink(fn)
+}
+
 get_national_polygons = function(geom, year, path) {
   
   url = paste0('https://files.hudexchange.info/resources/documents/',
@@ -51,10 +58,7 @@ get_national_polygons = function(geom, year, path) {
   dest_file = paste0('national_CoC_polygons_', year, '.zip')
   dest_path = file.path(path, dest_file)
   
-  download.file(url, dest_path)
-  rsleep()
-  
-  unzip(dest_path, exdir = path)
+  download_and_delete(url, dest_path, path)
   
 }
 
@@ -71,14 +75,9 @@ get_state_data = function(geom, year, path) {
   
   if (!dir.exists(unzipped_dir)) {
     dir.create(unzipped_dir, recursive=TRUE)
-    download.file(url, dest_path)
-    rsleep()
+    download_and_delete(url, dest_path, unzipped_dir)
     
   }
-  
-  unzip(dest_path, exdir = unzipped_dir)
-  
-  unlink(dest_path)
   
   
 }
@@ -111,14 +110,41 @@ get_hud_data = function(geom, FUN, year, path, error_file) {
 }
 
 
+fix_names = function(coc) {
+  
+  count_id = which(names(coc) == 'sh_pers_hw')
+  
+  if (length(count_id)>0) {
+    count_id = count_id[1]
+    
+    fixed_names = paste(
+      rep(c('sh', 'unsh'), 3),
+      'pers',
+      rep(c('hwoa', 'hwoc', 'hwac'), each=2),
+      sep='_'
+    )
+    
+    names(coc)[count_id:(count_id+5)] = fixed_names
+    
+  }
+  
+  return(coc)
+}
+
 
 read_in_state_cocs = function(fns) {
+  require('terra')
   
   coc_list = lapply(shape_fns, vect)
   
   coc = do.call(rbind, coc_list)
   
   names(coc) = tolower(names(coc))
+  
+  if (ncol(coc)>38) {
+    coc = fix_names(coc)
+    
+  }
   
   cols_to_remove = c('st', 'state_name', 'cocname', 'shape_leng', 'shape_area')
   
